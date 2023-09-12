015d27dd9dc3c299a6b172b8d041ee611eb7c20b
Jon Anderson <jon.anderson@willowtreeapps.com>
2023-09-12 16:01:16 -0400

    Starting an API server

--- index.ts
+++ index.ts
@@ -2,14 +2,31 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// create a new user
-await prisma.user.create({
-  data: {
-    name: "John Dough",
-    email: `john-${Math.random()}@example.com`,
+const server = Bun.serve({
+  port: 3000,
+  async fetch(request) {
+    const { pathname } = new URL(request.url);
+    const requestType = request.method;
+    let response;
+    if (pathname === "/users") {
+      const users = await prisma.user.findMany();
+      response = JSON.stringify(users);
+    } else {
+      response = "Try localhost:3000/users" + " method: " + requestType;
+    }
+    return new Response(response as any);
   },
 });
 
+console.log(`Listening on localhost: ${server.port}`);
+// create a new user
+// await prisma.user.create({
+//   data: {
+//     name: "John Dough",
+//     email: `john-${Math.floor(Math.random() * 2147483648)}@example.com`,
+//   },
+// });
+
 // count the number of users
-const count = await prisma.user.count();
-console.log(`There are ${count} users in the database.`);
+// const count = await prisma.user.count();
+// console.log(`There are ${count} users in the database.`);
